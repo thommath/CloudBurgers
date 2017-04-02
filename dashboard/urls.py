e53aa208cfb3eb13e4e625472854ab124aeddcbb
thommath <thomas-lm@vikenfiber.no>
2017-04-02 23:15:13 +0200

    started on setup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/', views.login, name='login'),
+    url(r'^login/', views.login_view, name='login'),
     url(r'^register/', views.register, name='register'),
+    url(r'^setup/', views.setup, name='setup'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Diner, UserWorksAtDiner, Table
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -10,8 +11,16 @@ def index(request):
     return render(request, 'dashboard/index.html', {})
 
 
+@login_required(login_url='/dashboard/login/')
+def map(request):
+
+    tables = Table.objects.filter()
+
+    return render(request, 'dashboard/map.html')
+
+
 # Create your views here.
-def login(request):
+def login_view(request):
     try:
         user = authenticate(username=request.POST['email'], password=request.POST['password'])
         if user is not None:
@@ -27,8 +36,44 @@ def login(request):
 
     #No username set
 
-
     return render(request, 'dashboard/login.html', {})
+
+def setup(request):
+    try:
+        if len(User.objects.filter(username='Admin')) != 0:
+            error_message = "Username already in use"
+            return render(request, 'splash/index.html', {'error_message': error_message})
+
+
+        user = User(username='Admin',
+                    first_name='admin',
+                    last_name='admin',
+                    email='admin')
+        user.set_password('admin')
+        user.save()
+        login(request, user)
+
+        diner = Diner(address='Langkaia 1',
+                      tlf='0985678908')
+        diner.save()
+
+        uwad = UserWorksAtDiner(user=user, diner=diner)
+        uwad.save()
+
+        t1 = Table(pos="0 0", diner=diner)
+        t1.save()
+        t2 = Table(pos="0 1", diner=diner)
+        t2.save()
+        t3 = Table(pos="0 2", diner=diner)
+        t3.save()
+        t4 = Table(pos="2 2", diner=diner)
+        t4.save()
+
+        return HttpResponseRedirect(reverse('dashboard:index'))
+
+    except(KeyError):
+        return render(request, 'splash/index.html', {})
+
 
 def register(request):
     try:
